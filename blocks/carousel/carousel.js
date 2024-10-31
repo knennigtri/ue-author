28282895828b42cf75e48a698e04d9f07c34cb68
Kevin Nennig <knennig213@gmail.com>
2024-10-31 14:00:09 -0400

    coursel update

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,21 +1,40 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+import createSlider from '../../scripts/slider.js';
+
 export default function decorate(block) {
-  const buttons = document.createElement('div');
-  buttons.className = 'carousel-buttons';
-  [...block.children].forEach((row, i) => {
-    const classes = ['image', 'text'];
-    classes.forEach((e, j) => {
-      row.children[j].classList.add(`carousel-${e}`);
-    });
-    /* buttons */
-    const button = document.createElement('button');
-    button.title = 'Carousel Nav';
-    if (!i) button.classList.add('selected');
-    button.addEventListener('click', () => {
-      block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
-      [...buttons.children].forEach((r) => r.classList.remove('selected'));
-      button.classList.add('selected');
-    });
-    buttons.append(button);
+  let i = 0;
+  const slider = document.createElement('ul');
+  const leftContent = document.createElement('div');
+  [...block.children].forEach((row) => {
+    if (i > 3) {
+      const li = document.createElement('li');
+      moveInstrumentation(row, li);
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
+      });
+      slider.append(li);
+    } else {
+      if (row.firstElementChild.firstElementChild) {
+        leftContent.append(row.firstElementChild.firstElementChild);
+      }
+      if (row.firstElementChild) {
+        leftContent.append(row.firstElementChild.firstElementChild || '');
+      }
+      leftContent.className = 'default-content-wrapper';
+    }
+    i += 1;
   });
-  block.parentElement.append(buttons);
+
+  slider.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.textContent = '';
+  block.parentNode.parentNode.prepend(leftContent);
+  block.append(slider);
+  createSlider(block);
 }
